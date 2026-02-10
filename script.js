1ee895897407fee6621f1bcb9803f9272477d8fe
Kaue Sehnem <kaauesehnem@gmail.com>
2026-02-10 18:09:36 -0300

    Small change 1

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ function toggleMode() {
 
     if (html.classList.contains('light')) {
         img.setAttribute("src", "assets/avatar-light2.png")
-        img.setAttribute("alt", "Foto de Kauê Sehnem normal(face) com óculos de sol, com camisa e fundo cinza claro")
+        img.setAttribute("alt", "Foto de Kauê Sehnem normal(face) com óculos de sol, com camisa preta e fundo cinza claro")
     } else {
         img.setAttribute("src", "assets/avatar2.png")
-        img.setAttribute("alt", "Foto de Kauê Sehnem normal(face), com camisa e fundo cinza claro")
+        img.setAttribute("alt", "Foto de Kauê Sehnem normal(face), com camisa preta e fundo cinza claro")
     }
 }
 
